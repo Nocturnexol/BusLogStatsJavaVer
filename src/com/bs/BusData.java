697ec87841d0757bbefff7a740ca41d7b1015553
Xyf <635653084@qq.com>
2017-08-23 09:56:48 +0800

    first commit

---
+++ src/com/bs/BusData.java
@@ -0,0 +1,13 @@
+package com.bs;
+
+import java.util.Date;
+
+class BusData {
+    String LineCode;
+    Direction Direction;
+    int StationNum;
+    String StationCode;
+    String VehCode;
+    Date EstimatedTime;
+    Date ProcessTime;
+}
